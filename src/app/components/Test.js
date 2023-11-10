31f993dde7654825ad49950542ef244b45ca26e5
Paulo Torrealba <contacto@volatil.cl>
2023-11-10 16:08:21 -0300

    limpiando codigo

--- src/app/components/Test.js
+++ src/app/components/Test.js
@@ -2,7 +2,6 @@
 
 import {API} from "../Helpers/CONST";
 import { useState } from "react";
-// import $ from "jquery";
 
 export default function Page() {
 	const [insignia, setInsignia] = useState("sininsignia");
@@ -16,8 +15,6 @@ export default function Page() {
 		const seleccionado = document.getElementById("select").value;
 		console.warn( seleccionado );
 		setInsignia( seleccionado )
-		// return seleccionado
-		// setinsignia( seleccionado );
 	}
 	
 	return (
@@ -26,9 +23,7 @@ export default function Page() {
 				<option value="estrellacentral">Estrella Central</option>
 				<option value="unionfoncea">Unión Foncea</option>
 			</select>
-			{/* <button onClick={algo}> JEJE </button> */}
 			<button onClick={lainsignia}> JEJE </button>
-			{/* <img src="../../../public/assets/images/clubes/colocolofarias.jpg" alt="Club" /> */}
 			<img src={`../assets/images/clubes/${insignia}.jpg`} alt="Club" />
 		</>
 	);
