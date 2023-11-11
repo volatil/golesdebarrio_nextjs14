e08a480bc0684e8fef147da82b593afe32e4d330
Paulo Torrealba <contacto@volatil.cl>
2023-11-11 03:57:43 -0300

    cuenta regresiva fix

--- src/components/Cuentaregresiva.js
+++ src/components/Cuentaregresiva.js
@@ -12,38 +12,48 @@ function Cuentaregresiva() {
 	
 	const [balonWidth, setBalonWidth] = useState();
 	const [balonLeft, setBalonLeft] = useState();
+	const [lacuentaregresiva, setlacuentaregresiva] = useState(" ");
+	const [cargando, setcargando] = useState(<Loading />);
 	
 	useEffect(() => {
-		const cuentaregresiva = function(){
-			const currentDate = new Date().getTime();
-			const timeLeft = TARGETDATE - currentDate;
-			if (timeLeft <= 0) {
-				clearInterval(interval);
-				document.querySelector('.cuenta').innerHTML = 'En curso';
-			} else {
-				const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-				const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-				const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-				const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-				const countdownString = `${days} días, ${hours} horas, ${minutes} minutos y ${seconds} segundos`;
-				document.querySelector('#lacuenta').innerHTML = countdownString;
-			}
-		}
 		
-		const avancebalon = function(){
+		const avancebalon = function(lacuentaregresiva){
 			let losdias_totales = 7;
-			let dias_restantes = document.querySelector("#lacuenta").innerHTML.split(" ")[0];
+			let dias_restantes = String( lacuentaregresiva ).split(" ")[0];
 			let dias_avanzados = losdias_totales-Number(dias_restantes);
 			let un_dia = 14.2;
 			let porcentaje_avanzado = dias_avanzados*un_dia;
 		
 			setBalonWidth( `${porcentaje_avanzado}%` );
-			setBalonLeft( `${porcentaje_avanzado}%` )
+			setBalonLeft( `${porcentaje_avanzado}%` );
 		}
+		
+		
+		const cuentaregresiva = function(){
+			const currentDate = new Date().getTime();
+			const timeLeft = TARGETDATE - currentDate;
+			if (timeLeft <= 0) {
+				clearInterval(interval);
+				// document.querySelector('.cuenta').innerHTML = 'En curso';
+				setlacuentaregresiva( "En curso" )
+				setcargando()
+			} else {
+				const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+				const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+				const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+				const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+				setlacuentaregresiva( `${days} días, ${hours} horas, ${minutes} minutos y ${seconds} segundos` )
+				setcargando()
+				return `${days} días, ${hours} horas, ${minutes} minutos y ${seconds} segundos`
+				
+			}
+		}
+		
 		
 		const interval = setInterval(() => {
 			cuentaregresiva();
-			avancebalon();
+			avancebalon( cuentaregresiva() )
 		}, 1000);
 	}, []);
 	
@@ -51,7 +61,8 @@ function Cuentaregresiva() {
 		<div className={`${all.bloque} ${inicio.cuentaregresiva}`}>
 			<p className={inicio.titulo}>Proximo encuentro en:</p>
 			<p id="lacuenta" className={ inicio.cuenta }>
-				<Loading />
+				{ lacuentaregresiva }
+				{cargando}
 			</p>
 			<span className={ inicio.rayita }></span>
 			<div className={ inicio.fechas }>
