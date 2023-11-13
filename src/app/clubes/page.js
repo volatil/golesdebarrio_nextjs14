3cd145e9ca4c8d529672c1506093f834bab21867
Paulo Torrealba <contacto@volatil.cl>
2023-11-13 02:01:41 -0300

    pisca de css

--- src/app/clubes/page.js
+++ src/app/clubes/page.js
@@ -1,15 +1,16 @@
 import Todoslosclubes from "@/components/Todoslosclubes";
-
-export async function generateMetadata({ params }) {
-	return {
-		title: "Clubes | Goles de Barrio",
-	}
-}
+import clubes from "@/styles/Clubes.module.css";
 
 function Clubes() {
 	return (
-		<section className="clubes">
-			<p>Home Clubes</p>
+		<section className={ clubes.clubes }>
+			<h2>CLUBES</h2>
+			
+			<div className={ clubes.elbuscadordeclubs }>
+				<input type="text" placeholder="Busca tu Club" />
+				<img src="/assets/images/optimized/buscar.webp" alt="Busca tu Club" />
+			</div>
+			
 			<Todoslosclubes />
 		</section>
 	);
--- src/components/Todoslosclubes.js
+++ src/components/Todoslosclubes.js
@@ -1,19 +1,19 @@
 import Link from 'next/link'
 import { CLUBES, insignia } from '../Helpers/CONST';
+import all from "@/styles/All.module.css";
+import clubes from "@/styles/Clubes.module.css";
 
 export default function Nav() {
 	return (
-		<div className="clubes">
+		<div className={ `${all.bloque} ${clubes.losclubes}` }>
 			{
 				CLUBES.map(club => {
 					const { id , nombre } = club;
 					return (
-						<div key={id}>
-							<Link href={`/clubes/${String(nombre).toLowerCase().replaceAll(" ", "-")}`}>
-								<img width="50px" src={ insignia( nombre ) } alt={ nombre } />
-								<p>{nombre}</p>
-							</Link>
-						</div>
+						<Link className={ clubes.club } href={`/clubes/${String(nombre).toLowerCase().replaceAll(" ", "-")}`} key={id}>
+							<img width="50px" src={ insignia( nombre ) } alt={ nombre } />
+							{/* <p>{nombre.toLowerCase()}</p> */}
+						</Link>
 					)
 				})
 			}
